b406351b17266b74748e62bc2521f412b235b71e
g0riglia <92130239+g0riglia@users.noreply.github.com>
2023-11-18 23:11:14 +0100

    Update calcolatrice.java

--- calcolatrice.java
+++ calcolatrice.java
@@ -16,14 +16,9 @@ public class calcolatrice {
 	}
 
 	public static void main(String[] args) {
-		//Inizializzazione dello Scanner
+		
 		Scanner src = new Scanner(System.in);
 		
-		//Dichiarazione variabili
-		byte scelta, n;
-		double risultato;
-		
-		//Richiesta dell'operazione
 		System.out.println("Scegli che operazione eseguire sapendo che:");
 		System.out.println("0 - Per eseguire un addizione.");
 		System.out.println("1 - Per eseguire una sottrazione.");
@@ -33,22 +28,20 @@ public class calcolatrice {
 		System.out.println("5 - Per eseguire una radice quadrata.");
 		System.out.println("6 - Per eseguire una percentuale.");
 		System.out.print("Scegli: ");
-		scelta = -1;
+		byte scelta = -1;
 		try { 
 			scelta = src.nextByte();
 		} catch (Exception e) {
 			System.out.println("Devi inserire un numero, non caratteri, simboli o parole/frasi.");
 		}
 		
-		//Creazione del menu
 		switch(scelta) {
 		    
-		    //0 - Addizione
 		    case 0:
-		    	risultato = 0;
+		    	double risultato = 0;
 		    	System.out.print("Inserisci la quantità di numeri da sommare: ");
-		    	n = src.nextByte();
-		    	double[] numeri = new double[100];
+		    	byte n = src.nextByte();
+		    	double[] numeri = new double[n];
 		    	for (byte i = 0; i < n; i++) {
 		    		System.out.print("Inserisci numero: ");
 		    		numeri[i] = src.nextDouble();
@@ -64,11 +57,10 @@ public class calcolatrice {
 		    	}
 		    	break;
 		    	
-		    //1 - Sottrazione
 		    case 1:
 		    	System.out.print("Inserisci la quantità di numeri da sottrarre: ");
 		    	n = src.nextByte();
-		    	double[] numeri2 = new double[100];
+		    	double[] numeri2 = new double[n];
 		    	for (byte i = 0; i < n; i++) {
 		    		System.out.print("Inserisci numero: ");
 		    		numeri2[i] = src.nextDouble();
@@ -85,12 +77,11 @@ public class calcolatrice {
 		    	}
 		    	break;
 		    	
-		    //2 - Moltiplicazione
 		    case 2:
 		    	risultato = 1;				//Stavolta 1 perché moltiplicare per 0 da sempre 0
 		    	System.out.print("Inserisci la quantità di numeri da moltiplicare: ");
 		    	n = src.nextByte();
-		    	double[] numeri3 = new double[100];
+		    	double[] numeri3 = new double[n];
 		    	for (byte i = 0; i < n; i++) {
 		    		System.out.print("Inserisci numero: ");
 		    		numeri3[i] = src.nextDouble();
@@ -106,11 +97,10 @@ public class calcolatrice {
 		    	}
 		    	break;
 		    	
-		    //3 - Divisione
 		    case 3: 
 		    	System.out.print("Inserisci la quantità di numeri da dividere: ");
 		    	n = src.nextByte();
-		    	double[] numeri4 = new double[100];
+		    	double[] numeri4 = new double[n];
 		    	for (byte i = 0; i < n; i++) {
 		    		System.out.print("Inserisci numero: ");
 		    		numeri4[i] = src.nextDouble();
@@ -127,7 +117,6 @@ public class calcolatrice {
 		    	}
 		    	break;
 		    
-		    //4 - Potenza
 		    case 4:
 		    	System.out.print("Inserisci la base: ");
 		    	short base = src.nextShort();
@@ -142,7 +131,6 @@ public class calcolatrice {
 		    	}
 		    	break;
 		    	
-		    //5 - Radice quadrata
 		    case 5: 
 		    	System.out.print("Inserisci il radicando (numero sotto la radice): ");
 		    	short radicando = src.nextShort();
@@ -155,7 +143,6 @@ public class calcolatrice {
 		    	}
 		    	break;
 		    	
-		    //6 - Percentuale
 		    case 6:
 		    	System.out.print("Inserisci il numero: ");
 		    	double numeropercentuale = src.nextDouble();
@@ -170,12 +157,10 @@ public class calcolatrice {
 		    	}
 		    	break;
 		    	
-		    //Default
 		    default:
 		    	System.out.println("Devi scegliere una tra quelle 6 opzioni.");
 		}
 		
-		//Fine del programma
 		System.out.println("Fine del programma.");
 		
 		//Nini :)
